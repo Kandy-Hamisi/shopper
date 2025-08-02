db4a09d797b6ac919122f5cde1e07a732f0d7af0
Kandy-Hamisi <hkandy2000@gmail.com>
2025-08-02 23:58:23 +0300

    added json middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, {json, urlencoded} from "express";
 import productsRoutes from "./routes/products/products.routes";
 
 const app = express();
 const port = 3000;
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
